43072184381ace2507cf62b042cec79016c0eca2
Arthur Souza <amdsouza@inf.ufpel.edu.br>
2022-11-05 14:43:46 -0300

    testes

--- app/bustracker/views.py
+++ app/bustracker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import Stop
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the bus tracker index.")
