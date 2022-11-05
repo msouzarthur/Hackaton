84822c6ea37e81ff3867fda03ba8f2799a6f1d39
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2022-11-05 18:12:07 -0300

    FEAT: Pagina inicial

--- app/bustracker/views.py
+++ app/bustracker/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def index(request):
-	return render(request, 'index.html')
-    #return HttpResponse("Hello, world. You're at the bus tracker index.")
+from datetime import date
+from .models import RouteStop
+
+def index(request, route=""):
+	if request.method == "POST":
+		context = {'selected_route':request.POST.get('route'),
+					'routes':['Selecionar rota', 'anglo - ru', 'anglo - famed'],
+					'date':request.POST.get('date'),
+					'routestops':RouteStop.objects.filter()
+				}
+		return render(request, 'index.html', context)
+	print(RouteStop.objects)
+	context = {'date': date.today().isoformat(),
+				'selected_route':"Selecionar rota",
+				'routes': ['Selecionar rota', 'Anglo', 'Capão do Leão'],
+				'routestops':RouteStop.objects
+				}
+	return render(request, 'index.html', context)
