a744016d89431e8ec0a5594624dde821b5c1e564
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2022-11-06 08:38:28 -0300

    FEAT: novos dados no banco

--- app/bustracker/admin.py
+++ app/bustracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bus, Route, Stop,  Driver, Student, RouteStop
+from .models import Bus, Route, Stop,  Driver, Student, RouteStop, Reservation
 
 # Register your models here.
 admin.site.register(Bus)
@@ -8,5 +8,6 @@ admin.site.register(Stop)
 admin.site.register(Driver)
 admin.site.register(Student)
 admin.site.register(RouteStop)
+admin.site.register(Reservation)
 
 
